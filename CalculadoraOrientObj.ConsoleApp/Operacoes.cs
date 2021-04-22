f35fbf7b743960bba4288b630194ed3a9eacdd0e
RuanMedTI <rurato2002@gmail.com>
2021-04-21 23:38:04 -0300

    Add project files.

---
+++ CalculadoraOrientObj.ConsoleApp/Operacoes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraOrientObj.ConsoleApp
+{
+    class Operacoes
+    {
+            public double primeiroNumero, segundoNumero;
+            public string historico;
+
+            public void operacaoMais()
+            {
+                double resultado = primeiroNumero + segundoNumero;
+                string conta = $"{primeiroNumero} + {segundoNumero} = {resultado}";
+                historico += conta + "\n";
+
+                Console.WriteLine(conta);
+            }
+
+            public void operacaoMenos()
+            {
+                double resultado = primeiroNumero - segundoNumero;
+                string conta = $"{primeiroNumero} - {segundoNumero} = {resultado}";
+                historico += conta + "\n";
+
+                Console.WriteLine(conta);
+            }
+
+            public void operacaoMultiplicacao()
+            {
+                double resultado = primeiroNumero * segundoNumero;
+                string conta = $"{primeiroNumero} * {segundoNumero} = {resultado}";
+                historico += conta + "\n";
+
+                Console.WriteLine(conta);
+            }
+
+            public void operacaoDivisao()
+            {
+                double resultado = primeiroNumero / segundoNumero;
+                string conta = $"{primeiroNumero} / {segundoNumero} = {resultado}";
+                historico += conta + "\n";
+
+                Console.WriteLine(conta);
+            }
+
+            public void visualizarContas()
+            {
+            Console.WriteLine(historico);
+            }
+    }
+}
---
+++ CalculadoraOrientObj.ConsoleApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace CalculadoraOrientObj.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "";
+            Operacoes operacoes = new Operacoes();
+
+            operacoes.primeiroNumero = 4;
+            operacoes.segundoNumero = 2;
+            operacoes.operacaoMais();
+            operacoes.operacaoMenos();
+            operacoes.operacaoMultiplicacao();
+            operacoes.operacaoDivisao();
+            
+            Console.WriteLine();
+
+            operacoes.visualizarContas();
+
+            Console.Clear();
+
+            while (true)
+            {
+                MostrarMenu();
+
+                opcao = Console.ReadLine();
+
+                if (OpcaoInvalida(opcao))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Opcao Invalida! Tente novamente.");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                do
+                {
+                    if (opcao == "4" && operacoes.segundoNumero == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Numero errado! Tente outro");
+                        Console.ResetColor();
+                        Console.ReadLine();
+                    }
+                } while (operacoes.segundoNumero == 0);
+
+                switch (opcao)
+                {
+                    case "1":
+                        operacoes.operacaoMais();
+                        break;
+
+                    case "2":
+                        operacoes.operacaoMenos();
+                        break;
+                    case "3":
+                        operacoes.operacaoMultiplicacao();
+                        break;
+                    case "4":
+                        operacoes.operacaoDivisao();
+                        break;
+                    case "5":
+                        operacoes.visualizarContas();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        private static void MostrarMenu()
+        {
+            Console.WriteLine("Calculadora Orientada a Objetos");
+            Console.WriteLine("Digite 1 para somar");
+            Console.WriteLine("Digite 2 para subtrair");
+            Console.WriteLine("Digite 3 para multiplicar");
+            Console.WriteLine("Digite 4 para dividir");
+            Console.WriteLine("Digite 5 para apresentar as operacoes");
+            Console.WriteLine("Digite S para sair");
+        }
+
+        private static bool OpcaoInvalida(string opcao)
+        {
+            return opcao != "1" && opcao != "2" && opcao != "3" &&
+            opcao != "4" && opcao != "5" && opcao != "S" && opcao != "s";
+        }
+    }
+}
